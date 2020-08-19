494aee44d4eedf155524e9ebd5ad261cb9abc57a
铜 关 <1229865681@qq.com>
2020-08-19 17:26:14 +0800

    右键功能添加

--- baoge/src/page/fan.vue
+++ baoge/src/page/fan.vue
@@ -26,22 +26,17 @@
         </tr>
       </table>
       <v-contextmenu ref="contextmenu">
-          <v-contextmenu-item @click="handleClick">取消</v-contextmenu-item>
-          <v-contextmenu-item @click="dialogFormVisible = true">菜单2</v-contextmenu-item>
-          <v-contextmenu-item>菜单3</v-contextmenu-item>
+          <v-contextmenu-item @click="handleClick">设置样板机</v-contextmenu-item>
+          <v-contextmenu-item @click="handleClick">取消样板机</v-contextmenu-item>
+          <v-contextmenu-item @click="dialogFormVisible = true">有功设定值</v-contextmenu-item>
+          <v-contextmenu-item @click="dialogFormVisible = true">无功设定值</v-contextmenu-item>
       </v-contextmenu>
       <!-- Form -->
 
-      <el-dialog title="收货地址" :visible.sync="dialogFormVisible">
+      <el-dialog title="有功设定值" :visible.sync="dialogFormVisible" width="30%" center="true">
         <el-form :model="form">
-          <el-form-item label="活动名称" :label-width="formLabelWidth">
-            <el-input v-model="form.name" autocomplete="off"></el-input>
-          </el-form-item>
-          <el-form-item label="活动区域" :label-width="formLabelWidth">
-            <el-select v-model="form.region" placeholder="请选择活动区域">
-              <el-option label="区域一" value="shanghai"></el-option>
-              <el-option label="区域二" value="beijing"></el-option>
-            </el-select>
+          <el-form-item label="设定值(KW)" :label-width="formLabelWidth">
+            <el-input v-model="form.name" placeholder="请输入值" autocomplete="off"></el-input>
           </el-form-item>
         </el-form>
         <div slot="footer" class="dialog-footer">
